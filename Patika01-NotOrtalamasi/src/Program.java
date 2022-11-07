8dba5a7f46a7890f9c2f84f009c815a6b01dcbb9
Tufan Erdoğan <tufanerdogn@gmail.com>
2022-11-07 21:34:12 +0300

    if-else kullanmadan Not Hesaplama

---
+++ Patika01-NotOrtalamasi/src/Program.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		String[] dersler = { "Matematik", "Fizik", "Kimya", "Türkçe", "Tarih", "Müzik" };
+		double sum = 0;
+		int temp = 0;
+		String info;
+		Scanner oku = new Scanner(System.in);
+
+		for (int i = 0; i < dersler.length; i++) {
+			System.out.print(dersler[i] + " Notunuzu giriniz: ");
+			temp = oku.nextInt();
+			info = (temp <= 100 && temp >= 0) ? ""
+					: "Girdiğiniz Not: " + temp + " ve not aralığında (0-100) değildir. Lütfen kontrol ederek tekrar ";
+			System.out.print(info);
+			sum += (temp <= 100 && temp >= 0) ? temp : 0;
+			i = (temp <= 100 && temp >= 0) ? i : i - 1;
+		}
+
+		sum = sum / dersler.length;
+		System.out.println("--------------------------------");
+		System.out.println((60 < sum) ? "Sınıfı geçti" : "Sınıfta Kaldı");
+	}
+}
