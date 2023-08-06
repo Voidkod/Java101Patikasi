a677f47ea1d57704e8e93fca5227daa52e75fbc9
Tufan Erdoğan <tufanerdogn@gmail.com>
2023-08-07 02:22:37 +0300

    Kullanici Girisi ve sifre yenileme calismasi

---
+++ Patika09Praitk-KullaniciGirisi/src/Program.java
@@ -0,0 +1,61 @@
+package stream;
+
+import java.util.Scanner;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		String userName="patika";
+		String password="Java101";
+		String inputUserName,inputPassword,inputSetPassword;
+		
+		
+		Scanner input = new Scanner(System.in);
+		
+		
+		boolean isTrue=true;
+		
+		while(isTrue) {
+			
+			System.out.print("Kullanıcı Adınız: ");
+			inputUserName=input.nextLine();
+			System.out.print("Şifreniz: ");
+			inputPassword=input.nextLine();
+			
+			if(userName.equals(inputUserName) && password.equals(inputPassword)){
+				System.out.println("Giriş Başarılı");
+				isTrue=false;
+			}
+			
+			else {
+				System.out.print("Kullanıcı adınız ve Şifreniz yanlıştır. Şifrenizi sıfırlamak isterseniz büyük harflerle 'EVET' yazın:");
+				if(input.nextLine().equalsIgnoreCase("EVET")) {
+					System.out.println("Kullanıcı Adınızı Giriniz: ");
+					if(input.nextLine().equalsIgnoreCase(userName)) {
+						System.out.println("Yeni Şifreniz eski şifrenizle aynı olmamalıdır.");
+						System.out.print("Yeni Şifrenizi giriniz: ");
+						inputSetPassword=input.nextLine();
+						if(inputSetPassword.equals(password)) {
+							System.out.println("Yeni Şifreniz eski şifrenizle aynı. Lütfen tekrar giriş yapınız.");
+						}
+						else {
+							password = inputSetPassword;
+							System.out.println("Şifreniz başarıyla oluşturuldu. Tekrar giriş yapabilirsiniz.");
+						}
+					}
+					else {
+						System.out.println("Kullanıcı bulunamadı. Lütfen daha sonra tekrar giriş yapmayı deneyiniz..");
+						isTrue=false;
+					}
+				}
+				else {
+					System.out.println("Lütfen daha sonra tekrar giriş yapmayı deneyiniz..");
+					isTrue=false;
+				}
+			}
+		}
+		
+	}
+
+}
