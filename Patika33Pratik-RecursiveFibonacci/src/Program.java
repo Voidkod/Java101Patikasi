4d5dd167d3dc61ac9e3d769a9bbef0c4f38f5cdb
Tufan Erdoğan <tufanerdogn@gmail.com>
2023-08-16 15:52:27 +0300

    Recursive ile Fibonacci Serisi Bulma

---
+++ Patika33Pratik-RecursiveFibonacci/src/Program.java
@@ -0,0 +1,38 @@
+//www.patika.dev
+//@author Tufan ERDOĞAN
+
+/* 
+	Java'da recursive metotlar ile fibonacci serisi bulan program yapıyoruz.
+*/
+
+import java.util.Scanner;
+
+public class Program {
+	
+	//int-String ve String-int dönüşümleriyle Palindrom sayı bulma.
+	
+	static int fib(int number) {
+		
+		if(number==1 || number==2) {
+			return 1;
+		}
+		return fib(number-1)+fib(number-2);
+		
+	}
+	
+    
+	public static void main(String[] args) {
+		
+		Scanner input=new Scanner(System.in);
+		System.out.print("Bir sayı giriniz : ");
+		int number= input.nextInt();
+		
+		for(int i=0; i<number; i++) {
+			if(i==0) System.out.println("Fibonacci "+(i+1)+". eleman :" + 0);
+			else System.out.println("Fibonacci "+(i+1)+". eleman :" + fib(i) );
+		}
+	
+		
+		  
+	}
+}
