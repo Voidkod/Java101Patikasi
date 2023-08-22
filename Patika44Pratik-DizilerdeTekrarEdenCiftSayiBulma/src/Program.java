8d337e0f0d8290f6477b4d91cfd320942de9dc18
Tufan Erdoğan <tufanerdogn@gmail.com>
2023-08-22 03:08:50 +0300

    Dizilerde tekrar eden çift sayı bulma(Güncellendi.)

--- Patika44Pratik-DizilerdeTekrarEdenCiftSayiBulma/src/Program.java
+++ Patika44Pratik-DizilerdeTekrarEdenCiftSayiBulma/src/Program.java
@@ -11,17 +11,20 @@ import java.util.Arrays;
 
 public class Program{
 	 public static void main(String[] args) {
-		int[] list = {1,2,23,6,2,5,5,8,9,8,4,2,10};
-		 int count=0;
+		int[] list = {1,2,23,6,2,5,5,8,9,8,4,2,10,50,50};
+		 int count=1;
 		 
 		Arrays.sort(list);
 		for(int i=0; i<list.length-1; i++) {
-			if(list[i]==list[i+1] && list[i]%2==0) {
+			if(list[i]==list[i+1] && list[i]%2==0 && i!=list.length-2) {
 				count++;
 			}
-			else if(count!=0) {
-				System.out.println(list[i]+" çift sayısı dizide "+(count+1)+" adet bulundu ve "+count+" kez tekrar etmiştir.");
-				count=0;
+			else if(i==list.length-2 && list[i]%2==0 && list[i]==list[i+1]){
+				System.out.println(list[i]+" çift sayısı dizide "+(count+1)+" kez tekrar etmiştir.");
+			}
+			else if(count!=1) {
+				System.out.println(list[i]+" çift sayısı dizide "+count+" kez tekrar etmiştir.");
+				count=1;
 			}
 			
 		}
